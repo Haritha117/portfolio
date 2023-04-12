893c3e9f58e2a8f4ced118a9e24a833d3d210817
Haritha <67861484+Haritha117@users.noreply.github.com>
2023-04-12 15:46:08 +0530

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">HARITHA </span>
+            from <span className="purple"> Coimbatore, Tamil Nadu.</span>
+            <br />I am a pursuing B.E Computer Science and Engineering in KPR Institute of Engineering and Technology .
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Gardening
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Photography
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">HARITHA</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
